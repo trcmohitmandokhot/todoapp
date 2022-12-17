8f063bde7424eebbbbf0556cb80ba453ce9df217
Mohit Mandokhot <mandokhotm@trcpg.com>
2022-12-17 14:03:57 -0500

    Model and View/Controller working

--- src/todo.py
+++ src/todo.py
@@ -1,8 +1,7 @@
-import sys
-from typing import List,Tuple
+# Todo App
 
-# Original Imports
-# from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+from typing import List,Tuple, Optional
 
 # Modified Imports
 from PyQt5.QtWidgets import QApplication, QMainWindow
@@ -11,29 +10,30 @@ from PyQt5.QtCore import QAbstractListModel, Qt, QModelIndex
 # Import UI
 from TodoMainWindow import Ui_todoMainWindow
 
+# This is the Model
 class TodoModel(QAbstractListModel):
-    def __init__(self, todos: List[Tuple[bool,str]] = None) -> None:
+    def __init__(self, todos: Optional[List[Tuple[bool,str]]] = None) -> None:
         super().__init__()
         self.todos = todos or []  # Data store. 
     
     # Handles requests for data from the view and returns appropriate result. 
     def data(self, index: QModelIndex, role: Qt.DisplayRole) -> str:
         "Return data for index row as requested by role."
-
         if role == Qt.DisplayRole:
             status, text = self.todos[index.row()]
-        return text
+            return text
 
-    def rowCount(self,index) -> int:
+    def rowCount(self,index: QModelIndex) -> int:
         # Get number of rows in current data.  
         return len(self.todos)
 
-# Multiple Inheritance(Why?) - QtDesigner generated .ui uses functions from MainWindow class, that need referenced. 
+
+# Main Window is the View/Controller(?) and connects the Model with View/Controller(?) 
 class MainWindow(QMainWindow, Ui_todoMainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
-        self.model = TodoModel()
+        self.model = TodoModel(todos = [(False,"Minstrone Soup"),(False,"Chicken Parmesan")])
         self.todoView.setModel(self.model)
 
 
