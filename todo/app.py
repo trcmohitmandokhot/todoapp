d5574f1e3b8d61404f98ebd2b23b0c9521a1aadd
trcmohitmandokhot <mohitmandokhot@gmail.com>
2023-11-05 13:24:53 -0500

    Updated version#, ran flake8 linter, black formatter

--- todo/app.py
+++ todo/app.py
@@ -1,6 +1,6 @@
 # Todo App
 
-__version__ = 0.1
+__version__ = 0.2
 __author__ = "Mohit Mandokhot"
 
 import os
@@ -18,31 +18,33 @@ from models.todo import TodoModel
 
 DATA_DIR = "./data"
 
-# MainWindow has all of the core stiching logic between model and view. 
+# MainWindow has all of the core stiching logic between model and view.
+
+
 class MainWindow(QMainWindow, Ui_todoMainWindow):
     def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
-        # self.model = TodoModel(todos = [(False,"Minstrone Soup"),(False,"Chicken Parmesan")])
         self.model = TodoModel()
         self.load()
         self.todoView.setModel(self.model)
-        # Connect add button signal to add_item action. 
+        # Connect add button signal to add_item action.
         self.addButton.clicked.connect(self.add_item)
         self.deleteButton.clicked.connect(self.delete_item)
         self.completeButton.clicked.connect(self.complete_item)
 
     def add_item(self) -> None:
         """
-        Get item from todoEdit, add to todos, then clear it.  
+        Get item from todoEdit, add to todos, then clear it.
         """
         # Get text entered in box
         item_text = self.todoEdit.text()
         item_status = False
         # Add to memory
         if item_text:
-            self.model.todos.append((item_status,item_text))
-            # Emit signal from model that shape of list i.e. layout has changed. This causes QListView to redraw. 
+            self.model.todos.append((item_status, item_text))
+            # Emit signal from model that shape of list i.e. layout has changed.
+            # This causes QListView to redraw.
             self.model.layoutChanged.emit()
             # Clear text entered in box
             self.todoEdit.clear()
@@ -50,63 +52,64 @@ class MainWindow(QMainWindow, Ui_todoMainWindow):
 
     def delete_item(self) -> None:
         """
-        Delete user selected item from todos, reflect it in list. 
+        Delete user selected item from todos, reflect it in list.
         """
-        # Get index of items selected by user. 
+        # Get index of items selected by user.
         user_sel = self.todoView.selectedIndexes()
-        # If no selections are active, do nothing. 
+        # If no selections are active, do nothing.
         if user_sel:
             user_sel_idx = user_sel[0].row()
             # Delete data from storage list
             self.model.todos.pop(user_sel_idx)
-            # Refresh view. 
+            # Refresh view.
             self.model.layoutChanged.emit()
             # Clear selection because index selected could be out of bounds...
-            # and a new selection from user is encouraged.  
+            # and a new selection from user is encouraged.
             self.todoView.clearSelection()
             self.save()
 
-    def complete_item(self) -> None: 
+    def complete_item(self) -> None:
         """
-        Mark user selected item as complete. 
+        Mark user selected item as complete.
         """
-        # Get index of items selected by user. 
+        # Get index of items selected by user.
         user_sel = self.todoView.selectedIndexes()
-        # If no selections are active, do nothing. 
+        # If no selections are active, do nothing.
         if user_sel:
             user_sel_idx = user_sel[0].row()
             user_sel_item = self.model.todos[user_sel_idx]
             user_sel_item_str = user_sel_item[1]
-            new_item = (True,user_sel_item_str)
+            new_item = (True, user_sel_item_str)
             # Delete old item and replace with new item
             self.model.todos.pop(user_sel_idx)
-            self.model.todos.insert(user_sel_idx,new_item)
-            # Refresh view. 
+            self.model.todos.insert(user_sel_idx, new_item)
+            # Refresh view.
             self.model.layoutChanged.emit()
-            # Clear selection 
+            # Clear selection
             self.todoView.clearSelection()
             self.save()
 
     def load(self):
-        " Load todo list from database"
+        "Load todo list from database"
         try:
-            with open(os.path.join(DATA_DIR,"user1.json"),"r") as f:
+            with open(os.path.join(DATA_DIR, "user1.json"), "r") as f:
                 self.model.todos = json.load(f)
         except FileNotFoundError as e:
             print(e)
         except json.JSONDecodeError as e:
-            if os.stat(f).st_size == 0: 
+            if os.stat(f).st_size == 0:
                 print("Empty database")
             else:
                 print(e)
 
     def save(self):
-        " Save modified todo list to database"
+        "Save modified todo list to database"
         try:
-            with open(os.path.join(DATA_DIR,"user1.json"),"w") as f:
-                data = json.dump(self.model.todos,f)
+            with open(os.path.join(DATA_DIR, "user1.json"), "w") as f:
+                json.dump(self.model.todos, f)
         except FileNotFoundError as e:
             print(e)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
